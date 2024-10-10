a696053c4a373c6c184cab64a0ce36512e89af88
BoujiDev <dev2024loper@gmail.com>
2024-10-10 20:25:03 +0330

    thank god

---
+++ chess.cpp
@@ -0,0 +1,307 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+ void print(int board[]){
+ 	int i=0;
+ 	string c[64]=
+	 {"RB","AB","FB","VB","KB","FB","AB","RB","SB","SB","SB","SB","SB","SB","SB","SB"
+	 ,"  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  "
+	 ,"  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ",
+	 "SW","SW","SW","SW","SW","SW","SW","SW","RW","AW","FW","VW","KW","FW","AW","RW"};
+	char cha = 'A';
+	cout<<endl;
+	for(int i=1; i<9; i++){
+		cout<<"  "<<i<<"  ";
+	}
+	cout<<endl;
+	{
+	cout<<char(218);
+	int aa=7;
+	while(aa--){
+	
+		int a=4;
+		while(a--){ 
+			cout<<char(196);
+		}
+		cout<<char(194);
+	}
+	
+	int a=4;
+		while(a--){
+			cout<<char(196);
+		}
+	cout<<char(191);
+	}
+	
+	int b=3;
+	while(b--){
+		cout<<endl;
+		{
+		
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);	
+			a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+		}
+		
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+			cout<<char(178)<<c[board[i++]]<<char(178)<<char(179);
+			cout<<char(176)<<c[board[i++]]<<char(176)<<char(179);
+		}cout<<"  "<<cha++;
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);
+			a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+		}
+		}
+		cout<<endl;	
+		{
+			cout<<char(195);
+			int a=7;
+			while(a--){
+				cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(197);
+			}
+			cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(180);
+			
+			
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+			a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);
+		}
+		}
+		cout<<endl;	
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+			cout<<char(176)<<c[board[i++]]<<char(176)<<char(179);
+			cout<<char(178)<<c[board[i++]]<<char(178)<<char(179);
+		}cout<<"  "<<cha++;
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+			a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);
+		}
+		}	
+		cout<<endl;
+		{
+		cout<<char(195);
+		int a=7;
+		while(a--){
+			cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(197);
+		}
+		cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(180);
+		
+		
+	}
+	}
+		cout<<endl;
+		{
+		
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);
+			a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+		}
+		
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+			cout<<char(178)<<c[board[i++]]<<char(178)<<char(179);
+			cout<<char(176)<<c[board[i++]]<<char(176)<<char(179);
+		}cout<<"  "<<cha++;
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);
+			a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+		}
+		}	
+		cout<<endl;
+		{
+			cout<<char(195);
+			int a=7;
+			while(a--){
+				cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(197);
+			}
+			cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(180);
+			
+			
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+			a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);
+		}
+		}	
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+			cout<<char(176)<<c[board[i++]]<<char(176)<<char(179);
+			cout<<char(178)<<c[board[i++]]<<char(178)<<char(179);
+		}cout<<"  "<<cha++;
+		}
+		cout<<endl;
+		{
+		cout<<char(179);
+		int aa=4;
+		while(aa--){
+		
+			int a=4;
+				while(a--){
+					cout<<char(176);
+				}
+			cout<<char(179);
+			a=4;
+				while(a--){
+					cout<<char(178);
+				}
+			cout<<char(179);
+		}
+		}	
+		cout<<endl;	
+		
+		{
+	cout<<char(192);
+	int aa=7;
+	while(aa--){
+	
+		int a=4;
+		while(a--){
+			cout<<char(196);
+		}
+		cout<<char(193);
+	}
+	int a=4;
+		while(a--){
+			cout<<char(196);
+		}
+	cout<<char(217);
+	}	
+	
+	}
+	
+
+int main() {
+	int a[64];
+	for(int i=0;i<64;i++)a[i]=i;
+		print(a);
+		while(1){
+			string s;
+			cout<<endl;
+			cin>>s;
+			int i=0, j=0,temp;
+			i=((s[0]-97)*8)+((s[1]-48)-1);
+			j=((s[4]-97)*8)+((s[5]-48)-1);
+			a[i]=i;
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+			system("cls");
+			print(a);
+		}
+	
+	
+}   
+
+	
+	
+	
+	
+	
+	
